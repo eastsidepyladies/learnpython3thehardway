d7074295262c93ef91180759dbcc5566d241d5a2
Kelly <colhtk@uw.edu>
2017-06-18 22:53:35 -0700

    ex9 done

---
+++ kellyc/ex9.py
@@ -0,0 +1,20 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# running the below in python2 does not respect the \n
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# adds contents of days/months variable to end of statement
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+# use of quotes allows you to break up code on separate lines - considered good ettiquette
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+We could maybe even take it farther.
+Perhaps to 9M?
+Maybe you should try this.
+I'll... uh... be waiting over heeeere.
+(See ya!)
+""")
