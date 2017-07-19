ee08442a3eeafa77da3e49e9b041b27b5a85a93f
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-07-19 01:27:11 -0700

    format strings

---
+++ ex5.py
@@ -0,0 +1,20 @@
+my_name = 'Zed A. Shaw'
+my_age =  115 # Now a Hobbit
+my_height = 36 # inches or 3 feet
+my_weight = 180 # better not skip second breakfast
+my_eyes = 'Green'
+my_teeth = 'Ivory'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print("Actually that's not too heavy for a hobbit.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the tea and pipe")
+
+#this line is tricky, try to get it exactly right
+
+total = my_age + my_height + my_weight
+
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
