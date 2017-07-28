d5aa94d498372b297f417ba1fa4d89c708b14d4f
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-07-27 23:28:24 -0700

    adding notes about escaping characters

--- ex10.py
+++ ex10.py
@@ -13,3 +13,24 @@ print(tabby_cat)
 print(persian_cat)
 print(backslash_cat)
 print(fat_cat)
+
+
+escape_sequence = """
+\\ \\\\ Backslash\n 
+\' \\' Single-quote(')\n 
+\" \\" Double quotes\n 
+\a \\a ASCII bell (BEL)\n 
+\b \\b ASCII backspace (BS)\n 
+\f \\f ASCII formfeed (FF)\n 
+\n \\n ASCII linefeed (LF) ie new line\n 
+\N{Black Chess Knight} \\N{name} Character named name in the Unicode database (Unicode only)\n 
+\r \\r Carrage return (CR)\n 
+\t \\t Horizontal tab (Tab)\n 
+\u2764 \\uxxxx Character with 16-bit hex value xxxx\n 
+\U0001da1d \\Uxxxxxxxx Character with 32-bit hex value xxxxxxxx\n 
+\v \\v ASCII vertical tab (VT)\n 
+\123 \\ooo Character with octal value ooo\n 
+\4A \\xhh Character with the hex value hh\n 
+"""
+
+print(escape_sequence)
