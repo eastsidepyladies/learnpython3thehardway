b6d9a33542528bceffda4e48374926afbda76599
audiblediner <colhtk@uw.edu>
2017-07-30 11:52:09 -0700

    python is a rollercoaster of fun

--- kellyc/ex14.py
+++ kellyc/ex14.py
@@ -4,7 +4,7 @@ script, user_name = argv
 prompt = '> '
 
 # Reminder: The f preceding string stands for format
-print(f"Hi {user_name} I'm the {script} script.")
+print(f"Hi {user_name}. I'm the {script} script.")
 print("I'd like to ask you a few questions.")
 print(f"Do you like me {user_name}?")
 likes = input(prompt)
@@ -21,19 +21,19 @@ You live in {lives}.
 And you have a {computer} computer.
 """)
 
+print(f"Nice to meet you, {user_name}.")
+print("Moving on, I just need to ask you a few quick questions in order to customize your rollercoaster ride. That is what you're here for, right?")
 
-print(f"Nice to meet you, {user_name}.)
-print("I just need to ask you a few quick questions in order to customize your rollercoaster ride.")
-
-print(f"On a scale of one to ten, with ten being petrified, how nervous are you?")
+print(f"Great. On a scale of one to ten, with ten being petrified, how nervous are you about this ride?")
 nervousness = input(prompt)
 
 print(f"Here are some headphones for you to wear, {user_name}. We can play music to pump you up or calm you down. What song would you like to hear?")
 song = input(prompt)
 
-print("What letter does your favorite rollercoaster track shape match?")
+print("What letter of the alphabet does your favorite rollercoaster track shape match? (Specify orientation if not upright.L)")
 track = input(prompt)
 
 print(f"""
-Woowee! {user_name}, you are in for a good time. Can you hear {song} in your ears yet? Good. Brace you for those {track}s! This ride will last {nervousness} minutes.
+Woowee! {user_name}, you are in for a good time. Can you hear {song} in your ears yet? Good. Brace yourself for those {track}s! This ride will last {nervousness} minutes.
 """)
+
