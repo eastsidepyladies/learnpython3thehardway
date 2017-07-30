653d8801e240cb60cb17060926f8870f22ad550c
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-07-29 19:49:52 -0700

    making program more interactive

--- ex16.py
+++ ex16.py
@@ -1,15 +1,31 @@
-from sys import argv
+import sys 
+import time
 
-script, filename = argv
+script, filename = sys.argv
 
 print("Today we are creating haiku's")
 print(f"We will save it in the file {filename} you provided")
 
-txt = open(filename)
-print("Currently your file contains the following: ")
-print(txt.read())
-txt.close()
+next = input(f'Do you want to see what is inside {filename}? Y or N: ')
 
+if next == 'Y':
+    
+    txt = open(filename)
+    print("Currently your file contains the following: ")
+    line = txt.readline()
+    
+    while (next != "N" and next != "n"):
+        print(line)
+        line = txt.readline()
+        sys.stdout.flush()
+        time.sleep(5)
+        print(line)
+        line = txt.readline()
+        sys.stdout.flush()
+        time.sleep(1)
+        next = input('More? Y or N: ')
+    txt.close()
+    
 print(f"We're going to erase {filename}.")
 print("If you don't want that hit CTRL-C (^C).")
 print("If you are ok with this, hit RETURN.")
@@ -17,12 +33,19 @@ print("If you are ok with this, hit RETURN.")
 input("?")
 
 print("Opening the file...")
+sys.stdout.flush()
 target = open(filename, 'w')
+time.sleep(5)
 
 print("Erasing the content in file. Goodbye!")
 target.truncate()
+sys.stdout.flush()
+time.sleep(5)
+
 
 print("Now I'm going to ask you to write a haiku")
+sys.stdout.flush()
+time.sleep(1)
 
 line1 = input("1st line (5 syllables): ")
 line2 = input("2nd line (7 syllables): ")
@@ -35,15 +58,17 @@ target.write("\n") #line break
 target.write(line2)
 target.write("\n") #line break
 target.write(line3)
+target.write("\n")
+
+print("And finally, we close it")
 target.close()
 
-print("Would you like me to read your new file back?")
-print("hit CTRL-C (^C). to close this program")
-print("otherwise, hit RETURN.")
-input("?")
+print("Would you like me to read your new file back?  Y or N: ")
+response = input(">")
 
-print("Here is your new file")
-
-txt = open(filename)
-print(txt.read())
-txt.close()
+if response == "y" or response == "Y":
+    print("Here is your new file")
+    
+    txt = open(filename)
+    print(txt.read())
+    txt.close()
