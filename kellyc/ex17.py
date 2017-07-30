44c125629f461a95be249125be8d934deeb426a6
audiblediner <colhtk@uw.edu>
2017-07-30 13:23:31 -0700

    ex17 done

--- kellyc/ex17.py
+++ kellyc/ex17.py
@@ -7,11 +7,11 @@ script, from_file, to_file = argv
 print(f"Copying from {from_file} to {to_file}")
 
 # in two lines
-in_file = open(from_file)
+in_file = open(from_file) 
 indata = in_file.read()
 
-# in one line - not sure how to do this... 
-# in_file = open(from_file,'r').read(indata)
+# above but in one line - not sure how to do this... 
+# in_file = open(from_file) ; indata = in_file.read()
 
 print(f"The input file is {len(indata)} bytes long.")
 print("Ready, hit RETURN to continue, ^C to abort.")
