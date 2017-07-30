4bffb8657927f04c9d9d39fbe31a4c21b8297cb1
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-07-29 20:17:39 -0700

    combining open and read into one line

--- ex17.py
+++ ex17.py
@@ -6,8 +6,8 @@ script, from_file, to_file = argv
 print(f"Copying from {from_file} to {to_file}")
 
 #we could do these two on one line, how?
-in_file = open(from_file)
-indata = in_file.read()
+indata = open(from_file).read()
+
 
 print(f"the input file is {len(indata)} bytes long")
 
@@ -23,4 +23,13 @@ out_file.write(indata)
 print("Alright, all done")
 
 out_file.close()
-in_file.close()
+
+""""
+#first make a sample filee
+echo "This is a test file." > test.txt
+# then look at it to make sure it created correctly
+cat test.txt
+
+#now run script
+python3.6 ex17.py test.txt new_file.txt
+"""
