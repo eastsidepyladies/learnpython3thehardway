873072b27c7cec4d26acfdeaa68856b43d16ddc3
audiblediner <colhtk@uw.edu>
2017-07-30 12:14:06 -0700

    ex15 done

---
+++ kellyc/ex15.py
@@ -0,0 +1,21 @@
+# argv allows you to pass command-line arguments to the script as a list
+from sys import argv
+
+# the two arguments in the list argv are the script and sample text files
+script, filename = argv
+
+# this creates a variable for sample text file, openned (not yet read)
+txt = open(filename)
+
+print(f"Here is your file {filename}.")
+print(txt.read())
+
+# this prompts user to supply sample text file again, unopenned
+print("Type the filename again:")
+file_again = input("What's the file called again? --> ")
+
+# takes input and gives it an opened status (not yet read)
+txt_again = open(file_again)
+
+# this reads and prints above
+print(txt_again.read())
