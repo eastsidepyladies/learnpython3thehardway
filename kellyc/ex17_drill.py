bd4e56e01b4d3c63538fb49698318ba6f6b83666
audiblediner <colhtk@uw.edu>
2017-07-31 16:04:46 -0700

    More Files

--- kellyc/ex17_drill.py
+++ kellyc/ex17_drill.py
@@ -1,4 +1,4 @@
-# Drill. I'm there are ways to shorten this more, but am not sure how other than to combine all rows using ;
+# Drill. I'm lost on how to shorten this without just combining lines with ; betwixt
 from sys import argv
 from os.path import exists
 
@@ -9,5 +9,3 @@ in_file = open(from_file) ; indata = in_file.read()
 out_file = open(to_file, 'w') ; out_file.write(indata)
 
 out_file,in_file.close()
-
-
