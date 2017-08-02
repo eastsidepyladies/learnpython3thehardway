1b08906b5e33f76c48fde944e101dff8247ec923
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-08-01 20:32:50 -0700

    Program to run ex25

---
+++ Jami/runex25.py
@@ -0,0 +1,20 @@
+import ex25
+sentence = "All good things come to those who wait"
+words = ex25.break_words(sentence)
+print(words)
+sorted_words = ex25.sort_words(words)
+print("\n\n"
+print(sorted_words)
+
+ex25.print_first_word(sorted_words)
+
+ex25.print_last_word(sorted_words)
+print("\n\n")
+print(sorted_words)
+sorted_words = ex25.sort_sentence(sentence)
+print("\n\n" )
+print(sorted_words)
+ex25.print_first_and_last(sentence)
+print("\n\n")
+ex25.print_first_and_last_sorted(sentence)
+print("\n\n")
