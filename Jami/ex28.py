6bf3c97948b13244a7775b3c2c9fd559c5336895
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-08-02 22:37:53 -0700

    Boolean Practice

---
+++ Jami/ex28.py
@@ -0,0 +1,21 @@
+print( True and True ) # True
+print( False and True ) # False
+print( 1 == 1 and 2 == 1 ) # False
+print( "test" == "test"  ) # True
+print( 1 == 1 or 2 != 1  ) # True
+print( True and 1 == 1  ) # True
+print( False and 0 != 0  ) # False
+print( True or 1 == 1 ) # True
+print( "test" == "testing" ) # False
+print( 1 != 0 and 2 == 1 ) # False
+print( "test" != "testing" ) # True
+print( "test" == 1 ) # False
+print("\n")
+print( not (True and False)   ) # True
+print( not (1 == 1 and 0 != 1)  ) # False
+print( not (10 == 1 or 1000 == 1000)  ) # False
+print( not (1 != 10 or 3 == 4)  ) # False
+print( not ("testing" == "testing" and "Zed" == "Cool Guy")  ) # True
+print( 1 == 1 and (not ("testing" == 1 or 1 == 0))  ) # True
+print( "chunky" == "bacon" and (not (3 == 4 or 3 == 3))  ) # False
+print( 3 == 3 and (not ("testing" == "testing" or "Python" == "Fun"))  ) # False
