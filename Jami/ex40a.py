7153f01c43cf8a2b538d34a41144bf113e2c9697
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-09-03 13:16:00 -0700

    instantiate a class

--- Jami/ex40a.py
+++ Jami/ex40a.py
@@ -1,7 +1,25 @@
 # mystuff = { 'apple': "I AM APPLES!"}
 # print (mystuff['apple']) # get apple from dict
 
-import mystuff
-mystuff.apple() # get apple from module
+# import mystuff
+# mystuff.apple() # get apple from module
 
-print(mystuff.tangerine) #get tangerine from module, only now its a variable
+# print(mystuff.tangerine) #get tangerine from module, only now its a variable
+
+# instead of import, you instantiate(create) a class in python
+
+class Mystuff(object):
+	
+	def __init__(self):
+		self.tangerine = "And now a thousand years between"
+		
+	
+	def apple (self):
+		print("I AM CLASSY APPLES!")
+		
+
+		
+# Instanciate an object
+thing = Mystuff()
+thing.apple()
+print(thing.tangerine)
