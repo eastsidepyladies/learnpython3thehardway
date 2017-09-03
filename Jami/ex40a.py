d8cf9403919a618dd3941b702acdafeff43ffc47
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-09-03 13:09:20 -0700

    bulding fruit module

--- Jami/ex40a.py
+++ Jami/ex40a.py
@@ -1,3 +1,7 @@
-mystuff = { 'apple': "I AM APPLES!"}
-print (mystuff['apple'])
+# mystuff = { 'apple': "I AM APPLES!"}
+# print (mystuff['apple']) # get apple from dict
 
+import mystuff
+mystuff.apple() # get apple from module
+
+print(mystuff.tangerine) #get tangerine from module, only now its a variable
