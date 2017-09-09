a7ff8ac3dbe692419d7bdb7186c88432903a3b99
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-09-08 23:30:04 -0700

    fixed files to pass tests

--- Jami/ex47/skeleton/setup.py
+++ Jami/ex47/skeleton/setup.py
@@ -11,7 +11,7 @@ config = {
 	'author_email': 'My email.',
 	'version': '0.1',
 	'install_requires': ['nose'], 
-	'packages': ['NAME'], 
+	'packages': ['ex47'], 
 	'scripts': [],
 	'name': 'projectname'
 }
--- Jami/ex47/skeleton/test/ex47_tests.py
+++ Jami/ex47/skeleton/test/ex47_tests.py
@@ -1,5 +1,5 @@
 from nose.tools import *
-import NAME
+import ex47
 
 def setup():
 	print("SETUP!")
