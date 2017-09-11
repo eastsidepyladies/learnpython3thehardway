320e53f5f3d7c66e6905d813167acca82ce781eb
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-09-11 16:58:31 -0700

    adding greet parameter

--- Jami/ex51/form_test.py
+++ Jami/ex51/form_test.py
@@ -7,9 +7,13 @@ app = Flask(__name__)
 @app.route("/hello")
 def index():
     name = request.args.get('name', 'Nobody')
+    greet = request.args.get('greet', 'Hi')
     
     if name:
-        greeting = f"Hello, {name}"
+        if greet:
+            greeting = f"{greet} {name}"
+        else: 
+            greeting = f"Hello, {name}"
     else:
         greeting = "Hi, there!"
         
@@ -20,5 +24,5 @@ if __name__ == "__main__":
     app.run()
     
 #C:/Python/python.exe form_test.py
-# opens http://localhost:5000/hello
+# opens http://localhost:5000/hello?name=Jose&greet=Hola
 
