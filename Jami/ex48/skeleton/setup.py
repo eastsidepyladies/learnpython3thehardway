08ac4bd76b19737be7eef0229fd3e6830d2fad21
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-09-11 14:45:10 -0700

    skeleton of exercise 48

---
+++ Jami/ex48/skeleton/setup.py
@@ -0,0 +1,19 @@
+try:
+	from setuptools import setuptools
+except ImportError:
+	from distutils.core import setup 
+	
+config = {
+	'description': 'My Project', 
+	'author': 'My name', 
+	'url': 'URL to get it at. ', 
+	'download_url': 'Where to download it.',
+	'author_email': 'My email.',
+	'version': '0.1',
+	'install_requires': ['nose'], 
+	'packages': ['ex48'], 
+	'scripts': [],
+	'name': 'projectname'
+}
+
+setup(**config)
