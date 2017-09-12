58d295f9dfdcaf2e8c48f39be51ae984b38bb7ff
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-09-11 17:03:30 -0700

    Exercise 51 redirects forms and greeting

--- Jami/ex51/app.py
+++ Jami/ex51/app.py
@@ -1,15 +1,27 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/hello", methods=['POST', 'GET'])
 def index():
-    greeting = "Howdy Partner!"
-    return render_template("index.html", greeting=greeting)
+
+    greeting = "Hello World" 
+    
+    if request.method == "POST":
+        name = request.form['name']
+        greet = request.form['greet']
+        greeting = f"{greet}, {name}"
+        return render_template("index.html", greeting=greeting)
+    
+    return render_template("hello_form.html")
     
     
 if __name__ == "__main__":
     app.run()
     
-#FLASK_APP=app.py flask run
+# C:/Python/python.exe app.py
+# FLASK_APP=app.py flask run
+# opens http://localhost:5000/
+
